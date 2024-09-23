83d1990bd8503b7e12c4899bd88a12e9673bc805
PHAMTHITUYET-BANBAN <pttuyetit@gmail.com>
2024-09-23 11:15:08 +0700

    capnhat

--- src/js/script_login.js
+++ src/js/script_login.js
@@ -1,7 +1,28 @@
-var modal = document.getElementById('id01');
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+// Ví dụ: Kiểm tra dữ liệu trước khi gửi form
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // Kiểm tra dữ liệu nhập vào
+  // ...
+  // Gửi form nếu dữ liệu hợp lệ
+});
+const rmCheck = document.getElementById("rememberMe"),
+    emailInput = document.getElementById("email");
+
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+  rmCheck.setAttribute("checked", "checked");
+  emailInput.value = localStorage.username;
+} else {
+  rmCheck.removeAttribute("checked");
+  emailInput.value = "";
 }
 
+function lsRememberMe() {
+  if (rmCheck.checked && emailInput.value !== "") {
+    localStorage.username = emailInput.value;
+    localStorage.checkbox = rmCheck.value;
+  } else {
+    localStorage.username = "";
+    localStorage.checkbox = "";
+  }
+}
